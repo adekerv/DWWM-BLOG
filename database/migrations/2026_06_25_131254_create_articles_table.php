35cf54d9d0fc60d6bae874b6c373a8b57d1fb0ea
Kervin Ade <adejkervin@gmail.com>
2026-06-26 12:23:48 -0400

    file adjustment and fixes

---
+++ database/migrations/2026_06_25_131254_create_articles_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Validation\Rules\Unique;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->string('title',255);
+            $table->string('slug',255) ->unique(); //Unique is a constant also known as addition property.
+            $table->longText('content');
+            $table->enum('status', ['draft','published'])->default('draft'); 
+            $table->timestamp('published_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('articles');
+    }
+};
