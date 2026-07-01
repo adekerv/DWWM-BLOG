b3330f02f74d58e276dac8f3587f3e5e31f7445c
Kervin Ade <adejkervin@gmail.com>
2026-07-01 10:35:02 -0400

    Completion of Category file, view, route

---
+++ resources/views/categories-list.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Liste de Categories</title>
+</head>
+</define>
+<body>
+    <h1>Categories</h1>
+    {{-- Verification --}}
+    @if($categories->isEmpty())
+        <p>Pani Category sorti la</p>
+    @else 
+        {{-- -Boucle to display the categories --}}
+        @foreach ($categories as $category)
+         <h2> {{ $category -> name }}</h2>
+            
+        @endforeach
+    @endif
+</body>
+</html>
